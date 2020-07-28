22d1851a06a50da50768fc7ec3beb1166127f4d0
Conor Mac Amhlaoibh <conormccauley1999@gmail.com>
2020-07-28 14:28:56 +0100

    Lots of animations done

---
+++ virtual_isl_dictionary/unity/HandController/Assets/Scripts/Hand Controller/Animations/AnimationClipTuple.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class AnimationClipTuple {
+
+    public Animation animation;
+    public string clipName;
+
+    public AnimationClipTuple(Animation animation, string clipName) {
+        this.animation = animation;
+        this.clipName = clipName;
+    }
+
+}
