c07cfaf19a39c1d0c1bb92d0554ea1cbe3c4758b
Owen Johnston <t-owjohn@microsoft.com>
2020-07-28 17:25:05 +0100

    Added navigation

--- virtual_isl_dictionary/lib/widgets/DailySuggestion.dart
+++ virtual_isl_dictionary/lib/widgets/DailySuggestion.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:virtual_isl_dictionary/pages/hand_page.dart';
 import 'package:weather/weather.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 
@@ -26,62 +27,69 @@ class _DailySuggestionState extends State<DailySuggestion> with TickerProviderSt
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      width: MediaQuery.of(context).size.width*.9,
-      child: Container(
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.all(Radius.circular(10)),
-          color: Colors.grey[300],
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Padding(padding: EdgeInsets.symmetric(vertical: 7.5),),
-            Container(
-              width: MediaQuery.of(context).size.width*.4,
-              height: MediaQuery.of(context).size.height*.035,
-              color: Colors.redAccent,
-              child: Center(
-                child: Text(
-                  "Daily Suggestion",
-                  style: TextStyle(color: Colors.white),
+    return GestureDetector(
+      onTap: () {
+        if(suggestedWord != null) {
+          Navigator.push(context, MaterialPageRoute(builder: (context) => HandPage(searchParameter: suggestedWord)));
+        }
+      },
+      child: SizedBox(
+        width: MediaQuery.of(context).size.width*.9,
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.all(Radius.circular(10)),
+            color: Colors.grey[300],
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Padding(padding: EdgeInsets.symmetric(vertical: 7.5),),
+              Container(
+                width: MediaQuery.of(context).size.width*.4,
+                height: MediaQuery.of(context).size.height*.035,
+                color: Colors.redAccent,
+                child: Center(
+                  child: Text(
+                    "Daily Suggestion",
+                    style: TextStyle(color: Colors.white),
+                  ),
                 ),
               ),
-            ),
-            suggestedWord != null ? Padding(
-              padding: EdgeInsets.all(MediaQuery.of(context).size.width*.04),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  Row(
-                    children: <Widget>[
-                      Padding(
-                        padding: EdgeInsets.fromLTRB(0,0,MediaQuery.of(context).size.width*0.05,0),
-                        child: Icon(Icons.wb_sunny, color: Colors.white, size: MediaQuery.of(context).size.width * 0.1,),
-                      ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Text(suggestedWord, style: TextStyle(color: Colors.white, fontSize: 25, fontWeight: FontWeight.bold),),
-                          Text("Looks like today's forecast is"+suggestedWord+"! \nLearn to sign this word", style: TextStyle(fontSize: 10),),
-                        ],
-                      ),
-                    ],
-                  ),
-                  Icon(Icons.navigate_next),
-                ],
+              suggestedWord != null ? Padding(
+                padding: EdgeInsets.all(MediaQuery.of(context).size.width*.04),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Row(
+                      children: <Widget>[
+                        Padding(
+                          padding: EdgeInsets.fromLTRB(0,0,MediaQuery.of(context).size.width*0.05,0),
+                          child: Icon(Icons.wb_sunny, color: Colors.white, size: MediaQuery.of(context).size.width * 0.1,),
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Text(suggestedWord, style: TextStyle(color: Colors.white, fontSize: 25, fontWeight: FontWeight.bold),),
+                            Text("Looks like today's forecast is"+suggestedWord+"! \nLearn to sign this word", style: TextStyle(fontSize: 10),),
+                          ],
+                        ),
+                      ],
+                    ),
+                    Icon(Icons.navigate_next),
+                  ],
+                ),
+              ) : Padding(
+                padding: EdgeInsets.all(50),
+                child: SpinKitThreeBounce(
+                  color: Colors.white,
+                  size: 20,
+                  controller: AnimationController(vsync: this, duration: const Duration(milliseconds: 1200)),
+                ),
               ),
-            ) : Padding(
-              padding: EdgeInsets.all(50),
-              child: SpinKitThreeBounce(
-                color: Colors.white,
-                size: 20,
-                controller: AnimationController(vsync: this, duration: const Duration(milliseconds: 1200)),
-              ),
-            ),
-          ],
+            ],
+          )
         )
-      )
+      ),
     );
   }
 
