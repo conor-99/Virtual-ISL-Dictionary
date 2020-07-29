3d3d1da73d5c21160ef467de8766cea445c5ea92
Owen Johnston <t-owjohn@microsoft.com>
2020-07-29 10:28:55 +0100

    Added bookmark functionality

--- virtual_isl_dictionary/lib/pages/bookmark_page.dart
+++ virtual_isl_dictionary/lib/pages/bookmark_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:virtual_isl_dictionary/widgets/CustomDrawer.dart';
 
 import '../User.dart';
+import 'hand_page.dart';
 
 class BookmarkPage extends StatefulWidget {
   User user;
@@ -16,13 +17,46 @@ class _BookmarkPageState extends State<BookmarkPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      drawer: CustomDrawer(),
       appBar: AppBar(
+        elevation: 0,
         iconTheme: IconThemeData(color: Colors.lightBlue[200]),
         backgroundColor: Colors.white,
         centerTitle: true,
         title: Text("Bookmarks", style: TextStyle(color: Colors.lightBlue[200]),),
       ),
+      body: ListView(
+        children: showBookmarks()
+      ),
     );
+  }
+
+  showBookmarks() {
+    List<ListTile> bookmarks = new List();
+    for(int i = 0; i < user.bookmarks.length; i++) {
+      bookmarks.add(ListTile(
+        onTap: () {
+          Navigator.push(context, MaterialPageRoute(builder: (context) => HandPage(searchParameter: user.bookmarks[i], user: this.user,)));
+        },
+        leading: IconButton(
+          onPressed: () {
+            deleteBookmark(user.bookmarks[i]);
+          },
+          icon: Icon(
+            Icons.delete,
+          ),
+        ),
+        title: Text(
+          user.bookmarks[i]
+        ),
+        trailing: Icon(Icons.navigate_next),
+      ));
+    }
+    return bookmarks;
+  }
+
+  deleteBookmark(String value) {
+    setState(() {
+      this.user.bookmarks.remove(value);
+    });
   }
 }
