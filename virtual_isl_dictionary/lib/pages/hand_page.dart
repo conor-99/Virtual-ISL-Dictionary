d31e3e6e1e5470c24ce18fe5d737e691ac0a3b7f
unknown <t-caobri@microsoft.com>
2020-07-30 15:05:06 +0100

    Added visual feedback

--- virtual_isl_dictionary/lib/pages/hand_page.dart
+++ virtual_isl_dictionary/lib/pages/hand_page.dart
@@ -13,10 +13,14 @@ class HandPage extends StatefulWidget {
   String searchParameter;
   bool isLearning;
   Challenge challenge;
-  HandPage({@required this.searchParameter, @required this.user, this.isLearning, this.challenge});
+  HandPage(
+      {@required this.searchParameter,
+      @required this.user,
+      this.isLearning,
+      this.challenge});
   @override
-  State<StatefulWidget> createState() =>
-      new _HandPageState(this.searchParameter, this.user, this.isLearning, this.challenge);
+  State<StatefulWidget> createState() => new _HandPageState(
+      this.searchParameter, this.user, this.isLearning, this.challenge);
 }
 
 class _HandPageState extends State<HandPage> with TickerProviderStateMixin {
@@ -31,6 +35,7 @@ class _HandPageState extends State<HandPage> with TickerProviderStateMixin {
   Challenge challenge;
   double percentComplete;
   Color testColor = Color(0xff64dd17);
+  Color loopButtonColor = Colors.lightBlue[200];
   TextEditingController textEditingController = new TextEditingController();
   AnimationController animationController;
   Animation colorAnimation;
@@ -38,22 +43,25 @@ class _HandPageState extends State<HandPage> with TickerProviderStateMixin {
 
   IconData playPauseIcon = Icons.play_arrow;
 
-  _HandPageState(this.searchParameter, this.user, this.isLearning, this.challenge);
+  _HandPageState(
+      this.searchParameter, this.user, this.isLearning, this.challenge);
 
   @override
   void initState() {
-    animationController = AnimationController(vsync: this, duration: Duration(milliseconds: 100));
+    animationController =
+        AnimationController(vsync: this, duration: Duration(milliseconds: 100));
     animationController.addListener(() {
       setState(() {
         print("update");
       });
     });
-    colorAnimation = ColorTween(begin: Color(0xff64dd17), end: Colors.redAccent).animate(animationController);
-    paddingAnimation = Tween<double>(begin: 0, end: 10).animate(animationController);
-    if(isLearning == null) {
+    colorAnimation = ColorTween(begin: Color(0xff64dd17), end: Colors.redAccent)
+        .animate(animationController);
+    paddingAnimation =
+        Tween<double>(begin: 0, end: 10).animate(animationController);
+    if (isLearning == null) {
       isLearning = false;
-    }
-    else {
+    } else {
       percentComplete = this.challenge.percentageComplete();
     }
     if (user.bookmarks.contains(searchParameter)) {
@@ -74,36 +82,45 @@ class _HandPageState extends State<HandPage> with TickerProviderStateMixin {
     return new Scaffold(
         appBar: AppBar(
           actions: <Widget>[
-            !isLearning ? IconButton(
-              icon: Icon(bookmark),
-              onPressed: setBookmark,
-            ) : Padding(
-              padding: EdgeInsets.fromLTRB(0, 0, MediaQuery.of(context).size.width*.06, 0),
-              child: Center(
-                child: Text(
-                  challenge.numberComplete().toString() + "/" + challenge.words.length.toString(),
-                  style: TextStyle(color: colorAnimation.value),
-                ),
-              ),
-            )
+            !isLearning
+                ? IconButton(
+                    icon: Icon(bookmark),
+                    onPressed: setBookmark,
+                  )
+                : Padding(
+                    padding: EdgeInsets.fromLTRB(
+                        0, 0, MediaQuery.of(context).size.width * .06, 0),
+                    child: Center(
+                      child: Text(
+                        challenge.numberComplete().toString() +
+                            "/" +
+                            challenge.words.length.toString(),
+                        style: TextStyle(color: colorAnimation.value),
+                      ),
+                    ),
+                  )
           ],
           backgroundColor: isLearning ? Colors.white : Colors.lightBlue[200],
-          title: !isLearning ? Text(
-            this.searchParameter,
-            style: TextStyle(color: Colors.white),
-          ) : Center(
-            child: LinearPercentIndicator(
-              width: MediaQuery.of(context).size.width*.65,
-              lineHeight: 14,
-              percent: this.percentComplete/100,
-              backgroundColor: Colors.grey[200],
-              progressColor: colorAnimation.value,
-              animation: true,
-              animationDuration: 200,
-            ),
-          ),
+          title: !isLearning
+              ? Text(
+                  this.searchParameter,
+                  style: TextStyle(color: Colors.white),
+                )
+              : Center(
+                  child: LinearPercentIndicator(
+                    width: MediaQuery.of(context).size.width * .65,
+                    lineHeight: 14,
+                    percent: this.percentComplete / 100,
+                    backgroundColor: Colors.grey[200],
+                    progressColor: colorAnimation.value,
+                    animation: true,
+                    animationDuration: 200,
+                  ),
+                ),
           centerTitle: true,
-          iconTheme: isLearning ? IconThemeData(color: Colors.lightBlue[200]) : IconThemeData(color: Colors.white),
+          iconTheme: isLearning
+              ? IconThemeData(color: Colors.lightBlue[200])
+              : IconThemeData(color: Colors.white),
         ),
         body: ListView(
           children: <Widget>[
@@ -124,9 +141,9 @@ class _HandPageState extends State<HandPage> with TickerProviderStateMixin {
                       height: MediaQuery.of(context).size.height * .7,
                     ),
                     Positioned(
-                      left:
-                          (MediaQuery.of(context).size.width - rotateWidgetWidth) /
-                              2,
+                      left: (MediaQuery.of(context).size.width -
+                              rotateWidgetWidth) /
+                          2,
                       top: MediaQuery.of(context).size.height * .02,
                       child: _apiController != null
                           ? RotateWidgetSlider(
@@ -185,72 +202,85 @@ class _HandPageState extends State<HandPage> with TickerProviderStateMixin {
                                 padding: EdgeInsets.symmetric(horizontal: 10),
                               ),
                               GestureDetector(
-                                  child:Icon(
+                                  child: Icon(
                                     Icons.repeat,
                                     size:
                                         MediaQuery.of(context).size.width * .08,
-                                    color: Colors.lightBlue[200],
-                                  ), onTap: () {
-                                if (isLooping == true) {
-                                  isLooping = false;
-                                } else {
-                                  isLooping = true;
-                                }
-                                _apiController.setLooping(isLooping);
-                              }),
+                                    color: loopButtonColor,
+                                  ),
+                                  onTap: () {
+                                    setState(() {
+                                      if (isLooping == true) {
+                                        isLooping = false;
+                                        loopButtonColor = Colors.lightBlue[200];
+                                      } else {
+                                        isLooping = true;
+                                        loopButtonColor = Colors.blue;
+                                      }
+                                      _apiController.setLooping(isLooping);
+                                    });
+                                  }),
                             ],
                           ),
-                          isLearning ? Padding(
-                            padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 4),
-                            child: Column(
-                              children: <Widget>[
-                                Container(
-                                  decoration: BoxDecoration(
-                                    color: Colors.grey[200],
-                                    borderRadius: BorderRadius.all(Radius.circular(5))
-                                  ),
-                                  height: MediaQuery.of(context).size.height*.045,
-                                  child: Padding(
-                                    padding: const EdgeInsets.symmetric(horizontal: 10),
-                                    child: TextField(
-                                      controller: textEditingController,
-                                      decoration: InputDecoration(
-                                        border: InputBorder.none,
-                                        hintText: "Guess the letter",
+                          isLearning
+                              ? Padding(
+                                  padding: const EdgeInsets.symmetric(
+                                      horizontal: 30, vertical: 4),
+                                  child: Column(
+                                    children: <Widget>[
+                                      Container(
+                                        decoration: BoxDecoration(
+                                            color: Colors.grey[200],
+                                            borderRadius: BorderRadius.all(
+                                                Radius.circular(5))),
+                                        height:
+                                            MediaQuery.of(context).size.height *
+                                                .045,
+                                        child: Padding(
+                                          padding: const EdgeInsets.symmetric(
+                                              horizontal: 10),
+                                          child: TextField(
+                                            controller: textEditingController,
+                                            decoration: InputDecoration(
+                                              border: InputBorder.none,
+                                              hintText: "Guess the letter",
+                                            ),
+                                          ),
+                                        ),
                                       ),
-                                    ),
+                                      Row(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.end,
+                                        children: <Widget>[
+                                          Padding(
+                                            padding: EdgeInsets.fromLTRB(0, 0,
+                                                paddingAnimation.value, 0),
+                                            child: RaisedButton(
+                                              child: Text("Continue"),
+                                              textColor: Colors.white,
+                                              color: colorAnimation.value,
+                                              onPressed: () {
+                                                guessWord(
+                                                    textEditingController.text);
+                                              },
+                                            ),
+                                          )
+                                        ],
+                                      )
+                                    ],
                                   ),
-                                ),
-                                Row(
-                                  mainAxisAlignment: MainAxisAlignment.end,
-                                  children: <Widget>[
-                                    Padding(
-                                      padding: EdgeInsets.fromLTRB(0, 0, paddingAnimation.value, 0),
-                                      child: RaisedButton(
-                                        child: Text("Continue"),
-                                        textColor: Colors.white,
-                                        color: colorAnimation.value,
-                                        onPressed: () {
-                                          guessWord(textEditingController.text);
-                                        },
-                                      ),
-                                    )
-                                  ],
                                 )
-                              ],
-                            ),
-                          )
-                          : RotateWidgetSlider(
-                            type: "playback",
-                            icon: Icons.fast_forward,
-                            title: "Playback Speed",
-                            valueUnit: "x",
-                            initialSliderValue: 1,
-                            width: MediaQuery.of(context).size.width * .9,
-                            apiController: _apiController,
-                            min: 0.25,
-                            max: 2,
-                          ),
+                              : RotateWidgetSlider(
+                                  type: "playback",
+                                  icon: Icons.fast_forward,
+                                  title: "Playback Speed",
+                                  valueUnit: "x",
+                                  initialSliderValue: 1,
+                                  width: MediaQuery.of(context).size.width * .9,
+                                  apiController: _apiController,
+                                  min: 0.25,
+                                  max: 2,
+                                ),
                         ],
                       )
                     : Container(),
@@ -280,25 +310,26 @@ class _HandPageState extends State<HandPage> with TickerProviderStateMixin {
 
   guessWord(String guess) {
     print("guess!");
-    if(guess.toLowerCase() == searchParameter.toLowerCase()) {
+    if (guess.toLowerCase() == searchParameter.toLowerCase()) {
       print("correct!");
       setState(() {
         this.challenge.completeWord(searchParameter);
         this.percentComplete = this.challenge.percentageComplete();
       });
-      if(this.percentComplete == 100) {
+      if (this.percentComplete == 100) {
         Navigator.pushReplacement(
             context,
             MaterialPageRoute(
                 builder: (context) => CompletedPage(
-                  challenge: this.user.challenges[0],
-                  user: this.user,
-                )));
+                      challenge: this.user.challenges[0],
+                      user: this.user,
+                    )));
       }
     } else {
       setState(() {
-        animationController.forward().whenComplete(() => animationController.reverse());
-
+        animationController
+            .forward()
+            .whenComplete(() => animationController.reverse());
       });
     }
   }
