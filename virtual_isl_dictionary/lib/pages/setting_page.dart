8c8b20fcc33dc0e3c9392fda6a116adef0e63a44
Breadman Tan <t-tayixi@microsoft.com>
2020-07-30 12:07:49 +0100

    Change text style

--- virtual_isl_dictionary/lib/pages/setting_page.dart
+++ virtual_isl_dictionary/lib/pages/setting_page.dart
@@ -14,81 +14,105 @@ class SettingPage extends StatelessWidget {
             style: TextStyle(color: Colors.lightBlue[200]),
           ),
         ),
-        body: DefaultTextStyle(
-          style: TextStyle(
-            fontSize: 40.0,
-            fontWeight: FontWeight.bold,
-            decoration: TextDecoration.underline,
-          ),
-          child: ListView(children: <Widget>[
-              Card(
-                  child: ExpansionTile(
-                title: Text("Sign Language"),
-                subtitle: Text("Irish Sign Language",
-                    style: TextStyle(
-                      fontSize: 13,
-                    )),
-                trailing: Icon(Icons.navigate_next),
+        body: ListView(children: <Widget>[
+          Card(
+              child: ExpansionTile(
+            title: Text("Sign Language",
+                style: TextStyle(
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                )),
+            subtitle: Text("Irish Sign Language",
+                style: TextStyle(
+                  fontSize: 13,
+                )),
+            trailing: Icon(Icons.navigate_next),
+            children: <Widget>[
+              Column(
                 children: <Widget>[
-                  Column(
-                    children: <Widget>[
-                      ListTile(title: Text("Irish Sign Language")),
-                      ListTile(title: Text("English Sign Language")),
-                      ListTile(title: Text("Spanish Sign Language"))
-                    ],
-                  )
+                  ListTile(title: Text("Irish Sign Language")),
+                  ListTile(title: Text("English Sign Language")),
+                  ListTile(title: Text("Spanish Sign Language"))
                 ],
-              )),
-              Card(
-                  child: ExpansionTile(
-                title: Text("Text Language"),
-                subtitle: Text(
-                  "English",
-                  style: TextStyle(fontSize: 13),
-                ),
-                trailing: Icon(Icons.navigate_next),
+              )
+            ],
+          )),
+          Card(
+              child: ExpansionTile(
+            title: Text("Text Language",
+                style: TextStyle(
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                )),
+            subtitle: Text(
+              "English",
+              style: TextStyle(fontSize: 13),
+            ),
+            trailing: Icon(Icons.navigate_next),
+            children: <Widget>[
+              Column(
                 children: <Widget>[
-                  Column(
-                    children: <Widget>[
-                      ListTile(title: Text("Irish")),
-                      ListTile(title: Text("English")),
-                      ListTile(title: Text("Spanish"))
-                    ],
-                  )
+                  ListTile(title: Text("Irish")),
+                  ListTile(title: Text("English")),
+                  ListTile(title: Text("Spanish"))
                 ],
-              )),
-              Card(
-                  child: ListTile(
-                title: Text("Review Terms of Service"),
-                trailing: Icon(Icons.navigate_next),
-              )),
-              Card(
-                  child: ListTile(
-                title: Text("Review Privacy Notice"),
-                trailing: Icon(Icons.navigate_next),
-              )),
-              Card(
-                  child: ListTile(
-                title: Text("Sign In"),
-                trailing: Icon(Icons.navigate_next),
-              )),
-              Card(
-                  child: ListTile(
-                title: Text("Create Account"),
-                trailing: Icon(Icons.navigate_next),
-              )),
-              Card(
-                  child: ListTile(
-                title: Text("Delete All Data"),
-                trailing: Icon(Icons.navigate_next),
-              )),
-              Card(
-                  child: ListTile(
-                title: Text("Request All Data"),
-                trailing: Icon(Icons.navigate_next),
-              )),
-            ]),
-          ),
-        );
+              )
+            ],
+          )),
+          Card(
+              child: ListTile(
+            title: Text("Review Terms of Service",
+                style: TextStyle(
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                )),
+            trailing: Icon(Icons.navigate_next),
+          )),
+          Card(
+              child: ListTile(
+            title: Text("Review Privacy Notice",
+                style: TextStyle(
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                )),
+            trailing: Icon(Icons.navigate_next),
+          )),
+          Card(
+              child: ListTile(
+            title: Text("Sign In",
+                style: TextStyle(
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                )),
+            trailing: Icon(Icons.navigate_next),
+          )),
+          Card(
+              child: ListTile(
+            title: Text("Create Account",
+                style: TextStyle(
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                )),
+            trailing: Icon(Icons.navigate_next),
+          )),
+          Card(
+              child: ListTile(
+            title: Text("Delete All Data",
+                style: TextStyle(
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                )),
+            trailing: Icon(Icons.navigate_next),
+          )),
+          Card(
+              child: ListTile(
+            title: Text("Request All Data",
+                style: TextStyle(
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                )),
+            trailing: Icon(Icons.navigate_next),
+          )),
+        ]));
   }
 }
