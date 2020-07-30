7116351c3e17adde2a3cf22bc6e7ef0d64307347
Breadman Tan <t-tayixi@microsoft.com>
2020-07-30 11:56:51 +0100

    Add the correct setting page

--- virtual_isl_dictionary/lib/pages/setting_page.dart
+++ virtual_isl_dictionary/lib/pages/setting_page.dart
@@ -3,54 +3,92 @@ import 'package:flutter/material.dart';
 class SettingPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ListView(
-  children: const <Widget>[
-    Card(child: ListTile(title: Text('One-line ListTile'))),
-    Card(
-      child: ListTile(
-        leading: FlutterLogo(),
-        title: Text('One-line with leading widget'),
-      ),
-    ),
-    Card(
-      child: ListTile(
-        title: Text('One-line with trailing widget'),
-        trailing: Icon(Icons.more_vert),
-      ),
-    ),
-    Card(
-      child: ListTile(
-        leading: FlutterLogo(),
-        title: Text('One-line with both widgets'),
-        trailing: Icon(Icons.more_vert),
-      ),
-    ),
-    Card(
-      child: ListTile(
-        title: Text('One-line dense ListTile'),
-        dense: true,
-      ),
-    ),
-    Card(
-      child: ListTile(
-        leading: FlutterLogo(size: 56.0),
-        title: Text('Two-line ListTile'),
-        subtitle: Text('Here is a second line'),
-        trailing: Icon(Icons.more_vert),
-      ),
-    ),
-    Card(
-      child: ListTile(
-        leading: FlutterLogo(size: 72.0),
-        title: Text('Three-line ListTile'),
-        subtitle: Text(
-          'A sufficiently long subtitle warrants three lines.'
+    return Scaffold(
+        appBar: AppBar(
+          elevation: 0,
+          iconTheme: IconThemeData(color: Colors.lightBlue[200]),
+          backgroundColor: Colors.white,
+          centerTitle: true,
+          title: Text(
+            "Settings",
+            style: TextStyle(color: Colors.lightBlue[200]),
+          ),
         ),
-        trailing: Icon(Icons.more_vert),
-        isThreeLine: true,
-      ),
-    ),
-  ],
-)
+        body: DefaultTextStyle(
+          style: TextStyle(
+            fontSize: 40.0,
+            fontWeight: FontWeight.bold,
+            decoration: TextDecoration.underline,
+          ),
+          child: ListView(children: <Widget>[
+              Card(
+                  child: ExpansionTile(
+                title: Text("Sign Language"),
+                subtitle: Text("Irish Sign Language",
+                    style: TextStyle(
+                      fontSize: 13,
+                    )),
+                trailing: Icon(Icons.navigate_next),
+                children: <Widget>[
+                  Column(
+                    children: <Widget>[
+                      ListTile(title: Text("Irish Sign Language")),
+                      ListTile(title: Text("English Sign Language")),
+                      ListTile(title: Text("Spanish Sign Language"))
+                    ],
+                  )
+                ],
+              )),
+              Card(
+                  child: ExpansionTile(
+                title: Text("Text Language"),
+                subtitle: Text(
+                  "English",
+                  style: TextStyle(fontSize: 13),
+                ),
+                trailing: Icon(Icons.navigate_next),
+                children: <Widget>[
+                  Column(
+                    children: <Widget>[
+                      ListTile(title: Text("Irish")),
+                      ListTile(title: Text("English")),
+                      ListTile(title: Text("Spanish"))
+                    ],
+                  )
+                ],
+              )),
+              Card(
+                  child: ListTile(
+                title: Text("Review Terms of Service"),
+                trailing: Icon(Icons.navigate_next),
+              )),
+              Card(
+                  child: ListTile(
+                title: Text("Review Privacy Notice"),
+                trailing: Icon(Icons.navigate_next),
+              )),
+              Card(
+                  child: ListTile(
+                title: Text("Sign In"),
+                trailing: Icon(Icons.navigate_next),
+              )),
+              Card(
+                  child: ListTile(
+                title: Text("Create Account"),
+                trailing: Icon(Icons.navigate_next),
+              )),
+              Card(
+                  child: ListTile(
+                title: Text("Delete All Data"),
+                trailing: Icon(Icons.navigate_next),
+              )),
+              Card(
+                  child: ListTile(
+                title: Text("Request All Data"),
+                trailing: Icon(Icons.navigate_next),
+              )),
+            ]),
+          ),
+        );
   }
 }
